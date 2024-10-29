2122231ee436c21cd1122b1cba7807136c58d3f5
Fati <125610455+fatiga99@users.noreply.github.com>
2024-10-29 18:06:04 -0300

    basic project arquitecture

---
+++ server/src/index.ts
@@ -0,0 +1,24 @@
+// import express, { Application, Request, Response } from 'express';
+// import cors from 'cors';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const app: Application = express();
+// const PORT = process.env.PORT || 5000;
+
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Rutas
+// import userRoutes from './src/routes/userRoutes';
+// import contactRoutes from './src/routes/contactRoutes';
+
+// app.use('/api/users', userRoutes);
+// app.use('/api/contacts', contactRoutes);
+
+// // Inicio del servidor
+// app.listen(PORT, () => {
+//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+// });
