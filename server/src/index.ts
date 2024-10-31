db818e55a240d98b971aa340210eb3beb0fa75a1
Fati <125610455+fatiga99@users.noreply.github.com>
2024-10-31 13:48:59 -0300

    .env configuration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,39 @@
-// import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 // import cors from 'cors';
-// import dotenv from 'dotenv';
+import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
 
-// dotenv.config();
+dotenv.config();
 
-// const app: Application = express();
-// const PORT = process.env.PORT || 5000;
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
 
-// // Middleware
+//db connection pool
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: Number(process.env.DB_PORT),
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+  });
+
+  export default pool;
+
+// Middleware
 // app.use(cors());
 // app.use(express.json());
 
 // // Rutas
-// import userRoutes from './src/routes/userRoutes';
-// import contactRoutes from './src/routes/contactRoutes';
+// // import userRoutes from './src/routes/userRoutes';
+// // import contactRoutes from './src/routes/contactRoutes';
 
 // app.use('/api/users', userRoutes);
 // app.use('/api/contacts', contactRoutes);
 
-// // Inicio del servidor
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-// });
+// Inicio del servidor
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
